f754b45148baba2eaa4290e40c61dc08bf63455b
cesurunsur <cesurunsur@gmail.com>
2019-11-09 13:44:23 -0500

    created Two new Python Files

---
+++ 12.py
@@ -0,0 +1,6 @@
+def average(nums):
+    average=sum(nums)/ len(nums)
+    print(f" The Average is {average}")
+
+
+average([1, 6, 8,43,546,65,7,676,8])
---
+++ 123.py
@@ -0,0 +1,8 @@
+print("hello user")
+name= input("What is Your Name ?")
+print(" Hello " + name)
+age=int(input ("How Old are You ?"))
+if (age<8):
+    print("aww !!!! You are just baby")
+else:
+  print("aww!! You are well traveled soul")
