ed21c6351ed8d04592c91fcdd9b63238012b1953
cesurunsur <cesurunsur@gmail.com>
2019-11-09 13:53:17 -0500

    def has been added to 123.py

--- 123.py
+++ 123.py
@@ -1,8 +1,11 @@
-print("hello user")
-name= input("What is Your Name ?")
-print(" Hello " + name)
-age=int(input ("How Old are You ?"))
-if (age<8):
-    print("aww !!!! You are just baby")
-else:
-  print("aww!! You are well traveled soul")
+def hello():
+    print("hello user")
+    name= input("What is Your Name ?")
+    print(" Hello " + name)
+    age=int(input ("How Old are You ?"))
+    if (age<8):
+        print("aww !!!! You are just baby")
+    else:
+        print("aww!! You are well traveled soul")
+
+hello()   
